9785e71859c4488c71fc98f98a3f175318f1f69b
Tommi Cox <tommicox@gmail.com>
2019-03-04 15:09:54 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,7 +6,7 @@ def line(deli)
  else
    current_line = "The line is currently:"
    deli.each.with_index(1) do |person, i|
-     current_line << "#{i}. #{person}"
+     current_line << " #{i}. #{person}"
 end
 puts current_line
 end
